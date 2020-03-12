6f461e391f57e5d5e3c35c687de64109d9325397
Paul Ciarlo <paul.ciarlo@gmail.com>
2020-03-11 20:11:29 -0400

    do not need these at least building Magisk e261579 with NDK 21.1.6210238

--- libbb/missing_syscalls.c
+++ libbb/missing_syscalls.c
@@ -54,10 +54,6 @@
 #if defined(ANDROID) || defined(__ANDROID__)
 /*# include <linux/timex.h> - for struct timex, but may collide with <time.h> */
 # include <sys/syscall.h>
-pid_t getsid(pid_t pid)
-{
-	return syscall(__NR_getsid, pid);
-}
 
 int stime(const time_t *t)
 {
@@ -65,6 +61,12 @@ int stime(const time_t *t)
 	tv.tv_sec = *t;
 	tv.tv_usec = 0;
 	return settimeofday(&tv, NULL);
+}
+
+#if 0
+pid_t getsid(pid_t pid)
+{
+	return syscall(__NR_getsid, pid);
 }
 
 int sethostname(const char *name, size_t len)
@@ -77,6 +79,7 @@ int adjtimex(struct timex *buf)
 {
 	return syscall(__NR_adjtimex, buf);
 }
+#endif
 
 int pivot_root(const char *new_root, const char *put_old)
 {
